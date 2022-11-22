9b0939ced1f35696d570c6a31c4af5af3b9ef56f
Atlant <cuz.den30@gmail.com>
2022-11-22 18:20:52 +0300

    gitignore

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите пятизначное число: ");
+try
+{
+    var num = Convert.ToInt32(Console.ReadLine());
+
+    if(num>99999 || num < 10000)
+    {
+        Console.WriteLine("Не пятизначное число");
+    }
+    else
+    {
+        var first = num/1000;
+        var second = num%100;
+        second = second%10*10 + second/10;
+
+        if (first == second )
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+        }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ zadacha21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите X1 ");
+var X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y1 ");
+var Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z1 ");
+var Z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите X2 ");
+var X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y2 ");
+var Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z2 ");
+var Z2 = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow((X1-X2),2) + Math.Pow((Y1-Y2),2) + Math.Pow((Z1-Z2),2));
+
+Console.WriteLine("Растояние между точками" + " " + result);
